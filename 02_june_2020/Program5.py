cb529915be8487852964f48b387fe32e2d694f4a
Aadesh <aadesh.shigavan01@gmail.com>
2020-06-04 20:20:12 +0530

    Nested List, tuple

---
+++ 02_june_2020/Program5.py
@@ -0,0 +1,10 @@
+my_list = [['Rohit Sharma','Sachine','Pollard'],['virat','chahal','ABD']]
+
+print(my_list[0][2])
+print(my_list[1][2])
+
+my_list[0].append("hardik")
+print(my_list)
+
+my_list.insert([1][0],"Raydu")
+print(my_list)
---
+++ 02_june_2020/Program6.py
@@ -0,0 +1,6 @@
+my_tuple = ("c","C++","Python","Java","C++")
+
+print(my_tuple.count("C++"))
+
+my_tuple.append("Android")
+print(my_tuple)
